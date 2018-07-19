6c78a04aada277d5523f50ec5d9e740b8a60ddb4
viorelvladut <vladutcrst@gmail.com>
2018-07-19 11:13:28 +0300

    update product path

--- src/products/models.py
+++ src/products/models.py
@@ -16,7 +16,7 @@ def upload_image_path(instance, filename):
     new_filename = random.randint(1, 2712316896)
     name, ext = get_filename_ext(filename)
     final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
-    return "products/{new_filename}/{final_filename}".format(
+    return "static_cdn/media_root/products/{new_filename}/{final_filename}".format(
             new_filename=new_filename,
             final_filename=final_filename
             )
